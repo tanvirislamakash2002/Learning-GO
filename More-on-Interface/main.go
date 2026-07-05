85a2474eed04d79e033436c09a256c511d250721
tanvirislamakash2002 <tanvirislamakash2002@gmail.com>
2026-07-06 00:59:40 +0600

    created mock payment method for testing purpose

--- More-on-Interface/main.go
+++ More-on-Interface/main.go
@@ -8,22 +8,54 @@ type PaymentMethod interface {
 type Bkash struct {
 	apiKey string
 }
+type Nagad struct {
+	apiKey string
+}
 
-func (bk Bkash) pay(amount float64) {
+func (bk *Bkash) pay(amount float64) {
 	fmt.Printf("Paying %.2f tk with Bkash", amount)
+}
+func (ng *Nagad) pay(amount float64) {
+	fmt.Printf("Paying %.2f tk with Nagad", amount)
 }
 
 type PaymentService struct {
 	method PaymentMethod
 }
 
+func NewPaymentService(method PaymentMethod) *PaymentService {
+	return &PaymentService{
+		method: method,
+	}
+
+}
+
+func NewNagad(apiKey string) *Nagad {
+	return &Nagad{
+		apiKey: apiKey,
+	}
+}
+
 func (ps PaymentService) checkout() {
 	ps.method.pay(100.00)
 }
+
+type MockPaymentMethod struct {
+}
+
+func (mockPm MockPaymentMethod) pay(amount float64) {
+	fmt.Println("Testing payment method")
+}
 func main() {
-	bkash := Bkash{apiKey: "4343hjkjj"}
-	paymentService := PaymentService{
-		method: bkash,
-	}
-	paymentService.checkout()
+	// bkash := Bkash{apiKey: "4343hjkjj"}
+	// paymentService1 := NewPaymentService(&bkash)
+	// paymentService1.checkout()
+
+	// nagad := NewNagad("ksddff83in")
+	// paymentService2 := NewPaymentService(nagad)
+	// paymentService2.checkout()
+
+	MockPm := MockPaymentMethod{}
+	paymentService3 := NewPaymentService(MockPm)
+	paymentService3.checkout()
 }
